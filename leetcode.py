f19940d5fee06a5fd8e16995b995f70a27bc8d44
Irshita <rkm.7387@gmail.com>
2026-06-07 11:55:03 +0530

    48: Rotate Image

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,19 @@
+# 48: Rotate image
+class Solution48:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+
+
 # 47: Permutation 2
 class Solution47:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
