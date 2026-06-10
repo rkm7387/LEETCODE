3ec99810a7b0920d5f7caf67d7d2d9d2adad88aa
Irshita <rkm.7387@gmail.com>
2026-06-10 20:57:57 +0530

    73: Set Matrix Zero

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,26 @@
+# 73: Set Matrix Zero
+class Solution73:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row = len(matrix)
+        col = len(matrix[0])
+
+        rowtrack = [0 for _ in range(row)]
+        coltrack = [0 for _ in range(col)]
+
+        for i in range(row):
+            for j in range(col):
+                if matrix[i][j] == 0:
+                    rowtrack[i] = -1
+                    coltrack[j] = -1
+        
+        for i in range(row):
+            for j in range(col):
+                if rowtrack[i] == -1 or coltrack[j] == -1:
+                    matrix[i][j] = 0
+
 # 6: zigzag
 class Solution6:
     def convert(self, s: str, numRows: int) -> str:
