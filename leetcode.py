509c9c675a900f07acb27f5ec1c0a247b8d94f9f
Irshita <rkm.7387@gmail.com>
2026-06-11 15:25:43 +0530

    20: Valid Paranthesis

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,25 @@
+# 20: Valid Paranthesis
+class Solution20:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for bracket in s:
+            if bracket == "(" or bracket == "{" or bracket == "[":
+                stack.append(bracket)
+            else:
+                if len(stack) == 0:
+                    return False
+                ch = stack.pop()
+                if (
+                    (bracket == ")" and ch == "(")
+                    or
+                    (bracket == "]" and ch == "[")
+                    or
+                    (bracket == "}" and ch == "{")):
+                    continue
+                else:
+                    return False
+        return len(stack) == 0
+
 # 8: ATOI
 class Solution8:
     def myAtoi(self, s: str) -> int:
