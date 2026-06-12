9eb155aafab857ea6942c02cbca8d46dfb84b5db
Irshita <rkm.7387@gmail.com>
2026-06-12 11:37:56 +0530

    17. Letter Combinations of a Phone Number

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,30 @@
+# 17. Letter Combinations of a Phone Number
+class Solution17:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "hi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backtrack(i+1, curStr + c)
+
+        if digits:
+            backtrack(0, "")
+
+        return res
+    
 # 59: Spiral Matrix 2
 class Solution59:
     def generateMatrix(self, n: int) -> List[List[int]]:
