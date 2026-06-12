4bb5bc9de4da601c39f5be6beab633745667e059
Irshita <rkm.7387@gmail.com>
2026-06-12 16:23:37 +0530

    66. Plus One

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,23 @@
+# 66. Plus One
+class Solution66:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits = digits[::-1]
+        one,i = 1,0
+
+        while one:
+            if i < len(digits):
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    one = 0
+            else:
+                digits.append(1)
+                one = 0
+            i += 1
+
+        return digits[::-1]
+
 # 17. Letter Combinations of a Phone Number
 class Solution17:
     def letterCombinations(self, digits: str) -> List[str]:
