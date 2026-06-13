5bdb98c2ea1a0e1b4d31d7b47cd92ed2e53830c1
Irshita <rkm.7387@gmail.com>
2026-06-13 15:26:46 +0530

    22. Generate Paranthesis

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,28 @@
+# 22. Generate Paranthesis
+class Solution22:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        res = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+
+            if openN < n:
+                stack.append("(")
+                backtrack(openN+1, closedN)
+                stack.pop()
+
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN+1)
+                stack.pop()
+
+        backtrack(0,0)
+
+        return res
+
 # 153:Find Minimum in Rotated Sorted Array
 class Solution153:
     def findMin(self, nums: List[int]) -> int:
