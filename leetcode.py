37be76f93ac3c06a54d02b5250150069aaf05530
Irshita <rkm.7387@gmail.com>
2026-06-13 09:43:02 +0530

    136: Single Number:

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,16 @@
+# 136: Single Number:
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums_dict = {}
+        for num in nums:
+            if num in nums_dict:
+                nums_dict[num] += 1
+            else:
+                nums_dict[num] = 1
+        for key,value in nums_dict.items():
+            if value == 1:
+                return key
+
 # 108: Sorted Array to BST
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
