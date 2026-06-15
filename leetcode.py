8910d70cb4c84301e90e9230de75a646f9109ccd
Irshita <rkm.7387@gmail.com>
2026-06-15 11:04:39 +0530

    4: Median of two sorted array

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,34 @@
+# 4: Median of two sorted array
+class Solution4:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        n1, n2 = len(nums1), len(nums2)
+        if n1 > n2:
+            return self.findMedianSortedArrays(nums2, nums1)
+        
+        n = n1 + n2
+        left = (n1 + n2 + 1) // 2  
+        low, high = 0, n1
+        
+        while low <= high:
+            mid1 = (low + high) // 2
+            mid2 = left - mid1
+            
+            l1 = float('-inf') if mid1 == 0 else nums1[mid1 - 1]
+            l2 = float('-inf') if mid2 == 0 else nums2[mid2 - 1]
+            r1 = float('inf') if mid1 == n1 else nums1[mid1]
+            r2 = float('inf') if mid2 == n2 else nums2[mid2]
+
+            if l1 <= r2 and l2 <= r1:
+                if n % 2 == 1:
+                    return float(max(l1, l2))
+                else:
+                    return (max(l1, l2) + min(r1, r2)) / 2.0
+            elif l1 > r2:
+                high = mid1 - 1
+            else:
+                low = mid1 + 1
+        return 0.0
+
 # 260: Single Number III
 class Solution260:
     def singleNumber(self, nums: List[int]) -> List[int]:
