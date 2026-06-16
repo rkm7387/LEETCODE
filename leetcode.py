4ec60e5acaeeff10fb926cf71e14cf5842520001
Irshita <rkm.7387@gmail.com>
2026-06-16 15:07:36 +0530

    12: Integer to Roman

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,19 @@
+# 12: Integer to Roman
+class Solution12:
+    def intToRoman(self, num: int) -> str:
+        symList = [["I", 1], ["IV", 4], ["V", 5], ["IX", 9], ["X", 10], ["XL", 40], ["L", 50], ["XC", 90], ["C", 100], ["CD", 400],["D", 500],["CM", 900],["M", 1000]]
+
+        res = ""
+
+        for sys, val in reversed(symList):
+            if num // val:
+                count = num // val
+                res += (sys * count)
+                num = num % val
+            
+        return res
+        
+
 # 240: Search in Matrix 2D II
 class Solution240:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
