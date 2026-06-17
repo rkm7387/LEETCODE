60f6a78dcc248f89dcff1345d68dbb2acd1ca5f1
Irshita <rkm.7387@gmail.com>
2026-06-17 17:09:16 +0530

    287: Find the duplicate number: Two Pointer Approach

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,19 @@
+# 287: Find the duplicate number
+class Solution287:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = 0,0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
+
 # 162: Find Peak Element
 class Solution162:
     def findPeakElement(self, nums: List[int]) -> int:
