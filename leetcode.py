d0b33781e17427f520fcd2bff67a382e28101d11
Irshita <rkm.7387@gmail.com>
2026-06-19 17:52:59 +0530

    1283: Find the smallest divisor

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,28 @@
+# 1283: Find the smallest divisor
+import math
+class Solution1283:
+    def callTotal(self,nums,n):
+        total = 0
+        for num in nums:
+            total = total + math.ceil(num/n)
+        return total
+
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        low = 1
+        high = max(nums)
+        ans = 0
+
+        while low <= high:
+            mid = (low + high)//2
+            total = self.callTotal(nums,mid)
+            if total <= threshold:
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        
+        return ans
+
 # 1482:Minimum number of days to make bouquet
 from typing import List
 class Solution1482:
