f866d9f44b7d1afd0e85b715936d383a23c802b7
Irshita <rkm.7387@gmail.com>
2026-06-19 12:53:56 +0530

    1482:Minimum number of days to make bouquet

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,37 @@
+# 1482:Minimum number of days to make bouquet
+from typing import List
+class Solution:
+    def canMakeBouquet(self, bloomday, m, k, day):
+        total = 0
+        count = 0
+        for i in range(len(bloomday)):
+            if bloomday[i] <= day:
+                count += 1
+                if count == k:
+                    total += 1
+                    count = 0
+            else:
+                count = 0
+        return total >= m
+
+
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        if m * k > len(bloomDay):
+            return -1
+        
+        low, high = min(bloomDay), max(bloomDay)
+        ans = -1
+
+        while low <= high:
+            mid = (low + high) // 2
+            if self.canMakeBouquet(bloomDay, m, k, mid):
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        
+        return ans
+
 # 875: Koko Eating Banana
 class Solution875:
     import math
