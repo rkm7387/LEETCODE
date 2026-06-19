072667b8c2dcb5b8e416aebbb70f63ada8477fea
Irshita <rkm.7387@gmail.com>
2026-06-19 12:11:41 +0530

    875: Koko Eating Banana

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,27 @@
+# 875: Koko Eating Banana
+class Solution875:
+    import math
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        low = 1
+        high = max(piles)
+        ans = -1
+        while low <= high:
+            mid = (low + high) // 2
+            total_hours = self.totalHours(piles, mid)
+            if total_hours <= h:
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        
+        return ans
+    
+    def totalHours(self, piles, hourly_rate):
+        total = 0
+        for i in range(len(piles)):
+            total = total + (math.ceil(piles[i]/hourly_rate))
+        return total
+
 # 7: reverse integer
 class Solution7:
     def reverse(self, x: int) -> int:
