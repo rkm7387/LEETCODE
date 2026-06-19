be6138b04c8f7599e8d43aa198511a118e9c7e4d
Irshita <rkm.7387@gmail.com>
2026-06-19 18:31:07 +0530

    1011: Capacity to ship packages within D days

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,32 @@
+# 1011: Capacity to ship packages within D days
+class Solution1011:
+    def find_days(self, weights, capacity):
+        total_days = 1
+        current_load = 0
+
+        for weight in weights:
+            if current_load  + weight > capacity:
+                total_days += 1
+                current_load = 0
+            current_load += weight
+        
+        return total_days
+
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        low = max(weights)
+        high = sum(weights)
+
+        while low <= high:
+            mid = (low + high) // 2
+            numberOfDays = self.find_days(weights,mid)
+            if numberOfDays <= days:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return low
+
+
 # 1283: Find the smallest divisor
 import math
 class Solution1283:
