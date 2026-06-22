b7674bc12a9d38ae8a74c25cd11d57a52c767f05
Irshita <rkm.7387@gmail.com>
2026-06-22 18:23:13 +0530

    51: N-Queens

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,41 @@
+# 51: N-Queens
+class Solution51:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        ans = []
+        board = ["." * n for _ in range(n)]
+
+        leftrow = [0] * n
+        upperDiagonal = [0] * (2*n-1)
+        lowerDiagonal = [0] * (2*n-1)
+
+        self.solve(0, board, ans, leftrow, upperDiagonal, lowerDiagonal, n)
+
+        return ans
+
+    
+    def solve(self, col, board, ans, leftrow, upperDiagonal, lowerDiagonal, n):
+        if col == n:
+            ans.append(board[:])
+            return
+        
+        for row in range(n):
+            if (
+            leftrow[row] == 0
+            and lowerDiagonal[row+col] == 0
+            and upperDiagonal[n-1+col-row] == 0
+            ):
+                board[row] = board[row][:col] + "Q" + board[row][col+1:]
+                leftrow[row] = 1
+                lowerDiagonal[row+col] = 1
+                upperDiagonal[n-1+col-row] = 1
+                self.solve(col+1, board, ans, leftrow,upperDiagonal, lowerDiagonal, n)
+
+                board[row] = board[row][:col] + "." + board[row][col+1 :]
+                leftrow[row] = 0
+                lowerDiagonal[row+col] = 0
+                upperDiagonal[n-1 + col - row] = 0
+        
+
 # 209: Minimum size of subarray sum
 class Solution209:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
