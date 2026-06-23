7b52c7f53c65c7658f13eca8a5bf1f5d54ed049f
Irshita <rkm.7387@gmail.com>
2026-06-23 10:14:40 +0530

    733: Flood Fill

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,33 @@
+# 733: Flood Fill
+class Solution733:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        if image[sr][sc] == color:
+            return image
+        
+        visited = deepcopy(image)
+        rows = len(visited)
+        cols = len(visited[0])
+        initial_color = visited[sr][sc]
+
+        queue = deque()
+        queue.append((sr,sc))
+
+        while len(queue) != 0:
+            i, j = queue.popleft()
+            visited[i][j] = color
+            for x,y in [(-1,0), (0,-1),(1,0), (0,1)]:
+                new_i = i + x
+                new_j = j + y
+                if new_i < 0 or new_i >= rows or new_j < 0 or new_j >= cols:
+                    continue
+                if visited[new_i] [new_j] != initial_color:
+                    continue
+                queue.append((new_i, new_j))
+        
+        return visited
+        
+        
+
 # 51: N-Queens
 class Solution51:
     def solveNQueens(self, n: int) -> List[List[str]]:
