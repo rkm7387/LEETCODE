65f65410072f52c2863563a090d03e7a20958f7b
Irshita <rkm.7387@gmail.com>
2026-07-02 21:14:53 +0530

    205: Isomorphic String

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,19 @@
+#  205: Isomorphic String
+class Solution205:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        mapST, mapTS = {}, {}
+        for i in range(len(s)):
+            c1,c2 = s[i], t[i]
+            if ((c1 in mapST and mapST[c1] != c2) or 
+            (c2 in mapTS and mapTS[c2] != c1)):
+                return False
+
+            mapST[c1] = c2
+            mapTS[c2] = c1
+
+        return True
+
+
 # 171: Excel Title Number
 class Solution171:
     def titleToNumber(self, columnTitle: str) -> int:
