19f73424575012fda70aa0aa3d19c94aed9d28fa
Irshita <rkm.7387@gmail.com>
2026-07-13 11:46:44 +0530

    451: Sorted Character by Frequency

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,17 @@
+# 451: Sorted Character by Frequency
+class Solution451:
+    def frequencySort(self, s: str) -> str:
+        result = ""
+        hash_map = {}
+
+        for ch in s:
+            hash_map[ch] = hash_map.get(ch,0) + 1
+
+        for ch, freq in sorted(hash_map.items(), key=lambda x: x[1], reverse=True):
+            result += ch * freq
+
+        return result
+
 # 796: Rotate a String
 class Solution796:
     def rotateString(self, s: str, goal: str) -> bool:
